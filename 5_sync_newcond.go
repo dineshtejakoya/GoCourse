fea772a5a6d34df9326eb40f4f0faec6ca7aba7c
Dinesh Teja Koya <koyadineshteja@gmail.com>
2025-09-14 23:45:26 +0530

    sync cond 28 min

--- 5_sync_newcond.go
+++ 5_sync_newcond.go
@@ -1,8 +1,81 @@
 package main
 
-//sync.NewCond is a function in Go's sync package that create a new condition variable
-//A condition variable is a synchronization primitive that allows goroutines to wait for certain conditions to be met while holding a lock,
-//It is used to signal one or more goroutines that some condition has changed
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+// sync.NewCond is a function in Go's sync package that create a new condition variable
+// A condition variable is a synchronization primitive that allows goroutines to wait for certain conditions to be met while holding a lock,
+// It is used to signal one or more goroutines that some condition has changed
+const bufferSize = 5
+
+type buffer struct {
+	items []int
+	mu    sync.Mutex
+	cond  *sync.Cond
+}
+
+func newBuffer(size int) *buffer {
+	b := &buffer{
+		items: make([]int, 0, size),
+	}
+	b.cond = sync.NewCond(&b.mu)
+	return b
+}
+
+func (b *buffer) produce(item int) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	for len(b.items) == bufferSize {
+		b.cond.Wait()
+	}
+
+	b.items = append(b.items, item)
+	fmt.Println("Produced:", item)
+	b.cond.Signal()
+}
+
+func (b *buffer) consume() int {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	for len(b.items) == 0 {
+		b.cond.Wait()
+		//This function stops doing anything and waits for other functions to append the slice
+	}
+
+	item := b.items[0]
+	b.items = b.items[1:]
+	fmt.Println("Consumed:", item)
+	b.cond.Signal()
+	return item
+}
+
+func producer(b *buffer, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := range 10 {
+		b.produce(i + 100)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func consumer(b *buffer, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for range 10 {
+		b.consume()
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func main() {
+	buffer := newBuffer(bufferSize)
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go producer(buffer, &wg)
+	go consumer(buffer, &wg)
+
+	wg.Wait()
 }
